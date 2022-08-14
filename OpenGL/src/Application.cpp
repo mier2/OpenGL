849069105aad2b5a68044c5c24efa6ff0ae85d2d
Mia Zhou <xiaoyazhou@ucsb.edu>
2022-08-14 14:26:22 -0700

    buffer created

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -30,6 +30,23 @@ int main(void)
         std::cout << "Error" << std::endl;
     }
 
+    //the buffer itself
+    float positions[6] = {
+        -0.5f, -0.5f,
+        0.0, 0.5f,
+        0.5f, -0.5f
+    };
+
+    //created the buffer
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+
+    //input the data
+    glBufferData(GL_ARRAY_BUFFER, 6*sizeof(float), positions, GL_STATIC_DRAW);
+
+
+
     //test
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -37,11 +54,8 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
         
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
+        glDrawArrays(GL_TRIANGLES, 0,3);
+        
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
